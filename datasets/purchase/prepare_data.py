eabeb44bef0015ba8ee735400806c468b26047a8
Vũ Hoàng Nhật Trường <hoangtruongnhatvu@gmail.com>
2025-10-25 04:16:08 +0000

    Truong Update distribution.py prepare_data.py

--- datasets/purchase/prepare_data.py
+++ datasets/purchase/prepare_data.py
@@ -1,4 +1,5 @@
 import os
+import json
 import numpy as np
 from sklearn.cluster import KMeans
 from sklearn.model_selection import train_test_split
@@ -20,3 +21,18 @@ if not os.path.exists(f'purchase{num_class}_train.npy'):
     X_train, X_test, y_train, y_test = train_test_split(data, label, test_size=0.2)
     np.save(f'purchase{num_class}_train.npy', {'X': X_train, 'y': y_train})
     np.save(f'purchase{num_class}_test.npy', {'X': X_test, 'y': y_test})
+
+# Cập nhật file dataset
+dataset_path = "datasets/purchase/datasetfile"
+
+if not os.path.exists(dataset_path):
+    dataset_info = {
+        "nb_train": len(X_train),
+        "nb_test": len(X_test),
+        "input_shape": [X_train.shape[1]],
+        "nb_classes": num_class,
+        "dataloader": "dataloader"
+    }
+
+    with open(dataset_path, "w") as f:
+        json.dump(dataset_info, f, indent=4)
