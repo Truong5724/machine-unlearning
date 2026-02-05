6a42da462f8fc3c846fa82d2720f74dda4adba57
Vũ Hoàng Nhật Trường <hoangtruongnhatvu@gmail.com>
2026-02-05 00:38:17 +0000

    Truong Update WideResNet Config

--- architectures/cifar10.py
+++ architectures/cifar10.py
@@ -42,9 +42,9 @@ class wide_basic(nn.Module):
 
 class Model(nn.Module):
     def __init__(self, input_shape, nb_classes, *args, **kwargs):
-        depth = 4
-        widen_factor = 1
-        dropout_rate = kwargs['dropout_rate'] if 'dropout_rate' in kwargs.keys() else 0.4
+        depth = 22
+        widen_factor = 4
+        dropout_rate = kwargs['dropout_rate'] if 'dropout_rate' in kwargs.keys() else 0.0
         num_classes = nb_classes
 
         super(Model, self).__init__()
--- example-scripts/cifar10-sharding/predict.sh
+++ example-scripts/cifar10-sharding/predict.sh
@@ -6,7 +6,7 @@ IFS=$'\n\t'
 shards=$1
 
 for i in $(seq 0 "$((${shards}-1))"); do
-    for j in 50 100 500; do
+    for j in 0; do
         echo "shard: $((${i}+1))/${shards}, number of unlearning requests: ${j}"
         python sisa.py --model cifar10 --test --dataset datasets/CIFAR-10/datasetfile --label "${j}" --batch_size 16 --container "cifar10" --shard "${i}"
     done
